b015dee95f04fffa2ca85cd5cfb8fd1bc1ef1543
orres <orres@orres-mbp13.local>
2020-07-04 01:54:42 +0800

    clean some rubbish

--- base_model.py
+++
@@ -1,9 +0,0 @@
-class BaseModel(object):
-    def __init__(self, N, X, Y, num_views):
-        self.N = N
-        self.X = X
-        self.Y = Y
-        self.num_views = num_views
-
-    def modeling(self):
-        pass
